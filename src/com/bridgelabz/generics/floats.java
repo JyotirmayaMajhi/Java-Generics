440ad0c1bbc07c8b93bbeaddc281a8cf3002abd1
JyotirmayaMajhi <jyotirmayamajhi1998@gamil.com>
2021-11-17 21:10:37 +0530

    Added maximum float number

---
+++ src/com/bridgelabz/generics/floats.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.generics;
+
+public class floats {
+	static Float[] arr = { 1.1f, 1.2f, 1.3f };
+
+	public static void main(String[] args) {
+		Float max;
+
+		max = findMaximum(arr[0], arr[1], arr[2]);
+		display();
+
+		System.out.println("Maximum number : "+max);
+
+	}
+
+	private static void display() {
+		for (Float a : arr) {
+			System.out.print(a + "  ");
+		}
+		System.out.println(); 
+	}
+
+	private static float findMaximum(Float a, Float b, Float c) {
+		Float max;
+		if (a.compareTo(b) > 0 || a.compareTo(c) > 0) {
+			max = a;
+		} else if (b.compareTo(c) > 0) {
+			max = b;
+		} else
+			max = c;
+		return max;
+		
+	}
+}
