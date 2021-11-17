42c16609f47970bd7278e488696b0a9400dff86e
JyotirmayaMajhi <jyotirmayamajhi1998@gamil.com>
2021-11-17 21:03:41 +0530

    Added maximum number at 2nd position

---
+++ src/com/bridgelabz/generics/findMaxNum.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.generics;
+
+public class findMaxNum {
+	static Integer[] arr = { 1, 3, 2 };
+
+	public static void main(String[] args) {
+		int max;
+
+		max = findMaximum(arr[0], arr[1], arr[2]);
+		display();
+
+		System.out.println("Maximum number : "+max);
+
+	}
+
+	private static int findMaximum(Integer a, Integer b, Integer c) {
+		int max;
+		if (a.compareTo(b) > 0 || a.compareTo(c) > 0) {
+			max = a;
+		} else if (b.compareTo(c) > 0) {
+			max = b;
+		} else
+			max = c;
+		return max;
+	}
+
+	private static void display() {
+		for (int a : arr) {
+			System.out.print(a + "  ");
+		}
+		System.out.println(); 
+	}
+}
